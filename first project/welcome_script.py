7e4f308cdea7a6511717715bdf0d805612578d92
Ryan Holgate <ryan.holgate@pursuit.org>
2025-06-15 13:50:52 -0400

    Added welcome script to first project

---
+++ first project/welcome_script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    # Print personalized welcome message from Ryan
+    welcome_message = "Hello, My name is Ryan I am glad to have you here!"
+    
+    print("\n" + "=" * len(welcome_message))
+    print(welcome_message)
+    print("=" * len(welcome_message))
+
+if __name__ == "__main__":
+    main()
